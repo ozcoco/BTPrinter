1ac08e2260cb26cc7822dccc0f02eb84f2540b87
ozcomcn <857527916@qq.com>
2019-03-01 01:23:59 +0800

    add fastBle

--- app/src/main/java/org/oz/PrinterActivity.java
+++ app/src/main/java/org/oz/PrinterActivity.java
@@ -1,14 +1,34 @@
 package org.oz;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import com.clj.fastble.BleManager;
+import com.clj.fastble.callback.BleScanCallback;
+import com.clj.fastble.data.BleDevice;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.provider.Settings;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class PrinterActivity extends AppCompatActivity {
 
@@ -22,6 +42,74 @@ public class PrinterActivity extends AppCompatActivity {
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show());
+
+        initBT();
+
     }
+
+
+    private void initBT() {
+
+        BleManager.getInstance().init(getApplication());
+
+        if (!BleManager.getInstance().isBlueEnable()) {
+
+            BleManager.getInstance().enableBluetooth();
+
+        }
+
+        BleManager.getInstance()
+                .enableLog(true)
+                .setReConnectCount(1, 5000)
+                .setConnectOverTime(20000)
+                .setOperateTimeout(5000);
+
+        checkPermissions();
+
+    }
+
+
+    private static final int REQUEST_CODE_PERMISSION_LOCATION = 2;
+
+    private void checkPermissions() {
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (!bluetoothAdapter.isEnabled()) {
+            Toast.makeText(this, "请先打开蓝牙", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION};
+        List<String> permissionDeniedList = new ArrayList<>();
+        for (String permission : permissions) {
+            int permissionCheck = ContextCompat.checkSelfPermission(this, permission);
+            if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+                onPermissionGranted(permission);
+            } else {
+                permissionDeniedList.add(permission);
+            }
+        }
+        if (!permissionDeniedList.isEmpty()) {
+            String[] deniedPermissions = permissionDeniedList.toArray(new String[permissionDeniedList.size()]);
+            ActivityCompat.requestPermissions(this, deniedPermissions, REQUEST_CODE_PERMISSION_LOCATION);
+        }
+    }
+
+
+    private void onPermissionGranted(String permission) {
+        switch (permission) {
+            case Manifest.permission.ACCESS_FINE_LOCATION:
+
+                break;
+        }
+    }
+
+
+    private boolean checkGPSIsOpen() {
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        if (locationManager == null)
+            return false;
+        return locationManager.isProviderEnabled(android.location.LocationManager.GPS_PROVIDER);
+    }
+
 
 }
--- app/src/main/java/org/oz/PrinterActivityFragment.java
+++ app/src/main/java/org/oz/PrinterActivityFragment.java
@@ -1,13 +1,13 @@
 package org.oz;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
@@ -15,16 +15,23 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.clj.fastble.BleManager;
+import com.clj.fastble.callback.BleScanCallback;
+import com.clj.fastble.data.BleDevice;
+import com.clj.fastble.scan.BleScanRuleConfig;
 import com.yf.btp.PrinterService;
 
 import org.oz.databinding.FragmentPrinterBinding;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
@@ -41,8 +48,6 @@ public class PrinterActivityFragment extends Fragment {
 
     private Intent intentPrinterService;
 
-    private BluetoothAdapter mBluetoothAdapter;
-
     public class Handles {
 
         public void onPrinter(View v) {
@@ -54,14 +59,38 @@ public class PrinterActivityFragment extends Fragment {
 
             Log.i("^_*>>>", "开始扫描……");
 
-            mBluetoothAdapter.startDiscovery();
+
+
+            BleManager.getInstance().scan(new BleScanCallback() {
+                @Override
+                public void onScanFinished(List<BleDevice> scanResultList) {
+
+
+                }
+
+                @Override
+                public void onScanStarted(boolean success) {
+
+                }
+
+                @Override
+                public void onScanning(BleDevice bleDevice) {
+
+                    contactBtStr(bleDevice);
+
+                    mBinding.setContent(btStrBuilder.toString());
+
+                }
+            });
+
+
         }
 
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_printer, container, false);
@@ -78,14 +107,6 @@ public class PrinterActivityFragment extends Fragment {
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
 
-        // Register the BroadcastReceiver
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        filter.addAction(BluetoothDevice.ACTION_FOUND);
-        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
-        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-
-        Objects.requireNonNull(getActivity()).registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
-
         bindService();
     }
 
@@ -93,7 +114,6 @@ public class PrinterActivityFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        initBT();
 
         initData();
 
@@ -102,45 +122,16 @@ public class PrinterActivityFragment extends Fragment {
     }
 
 
-    private int REQUEST_ENABLE_BT = 0x01011;
-
     private StringBuilder btStrBuilder = new StringBuilder();
 
-    private void initBT() {
 
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        if (mBluetoothAdapter != null) {
-
-            if (!mBluetoothAdapter.isEnabled()) {
-
-                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-
-                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-            }
-
-
-            Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-            // If there are paired devices
-            if (pairedDevices.size() > 0) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
-                    pairedDevices.forEach(this::contactBtStr);
-                else
-                    for (BluetoothDevice device : pairedDevices)
-                        contactBtStr(device);
-            }
-
-        }
-
-    }
-
-
-    private void contactBtStr(BluetoothDevice device) {
+    private void contactBtStr(BleDevice device) {
 
         btStrBuilder.append("Device Name :").append(device.getName()).append('\n');
 
-        btStrBuilder.append("Device Address :").append(device.getAddress()).append('\n').append('\n').append('\n');
+        btStrBuilder.append("Device Rssi :").append(device.getRssi()).append('\n');
 
+        btStrBuilder.append("Device Address :").append(device.getMac()).append('\n').append('\n').append('\n');
 
     }
 
@@ -152,73 +143,14 @@ public class PrinterActivityFragment extends Fragment {
 
     private void initView() {
 
-        mBinding.setContent(btStrBuilder.toString());
 
     }
-
-    private StringBuilder discoveryBtStrBuilder = new StringBuilder();
-    // Create a BroadcastReceiver for ACTION_FOUND
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-
-            Log.i("^_*>>>", "接收扫描广播……");
-
-            String action = intent.getAction();
-
-            Log.i("^_*>>>", "扫描广播action：" + action);
-
-            // When discovery finds a device
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Get the BluetoothDevice object from the Intent
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                // Add the name and address to an array adapter to show in a ListView
-
-                discoveryBtStrBuilder.append("Device Name :").append(device.getName()).append('\n');
-
-                discoveryBtStrBuilder.append("Device Detail :").append(device.toString()).append('\n');
-
-                discoveryBtStrBuilder.append("Device Address :").append(device.getAddress()).append('\n').append('\n').append('\n');
-
-
-                Objects.requireNonNull(getActivity()).runOnUiThread(() -> {
-
-                    mBinding.setDiscovery(discoveryBtStrBuilder.toString());
-
-                });
-
-                Log.e("^_*>>>", discoveryBtStrBuilder.toString());
-
-                if (device.getName().contains("1324D")) {
-
-                    if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
-
-                        device.setPin("0000".getBytes());
-
-                        if (device.createBond()) {
-
-                        }
-
-                    }
-                }
-
-
-            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(intent.getAction())) {
-                //todo
-
-
-            }
-
-        }
-    };
 
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (REQUEST_ENABLE_BT == requestCode) {
-
-        }
 
     }
 
@@ -232,7 +164,7 @@ public class PrinterActivityFragment extends Fragment {
     public void onDestroy() {
         super.onDestroy();
 
-        Objects.requireNonNull(getActivity()).unregisterReceiver(mReceiver);
+        BleManager.getInstance().destroy();
 
         unbindService();
 
